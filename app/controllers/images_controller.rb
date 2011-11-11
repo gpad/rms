cf57fa05c6ca90ca46f87437879a8240d7ca909e
Gianluca Padovani <gpad@gpad-travel.(none)>
2011-11-11 16:38:30 +0100

    possibilità di fare upload di nuovi file

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,17 +1,33 @@
-class ImageInfo
-	attr_reader :src
-	attr_reader :name
-	def initialize(path, name)
-		@src = File.join(path, name)
-		@name = name
-	end
+class ImagesController < ApplicationController
+  def index
+    img_root = File.join(Rails.root, 'public/images')
+      @images = Dir.entries(img_root).select{|img| !File.directory?(img)}.collect do |img| 
+        ImageInfo.new(img_root, img)
+      end
+  end
+  
+  def upload
+    uploaded_io = params[:picture]
+    if uploaded_io
+      File.open(Rails.root.join('public', 'images', uploaded_io.original_filename), 'wb') do |file|
+        file.write(uploaded_io.read)
+      end
+    end
+    redirect_to '/'
+  end
+
+
+  def show
+  end
 end
 
-class ImagesController < ApplicationController
-	def index
-		img_root = File.join(Rails.root, 'public/images')
-		@images = Dir.entries(img_root).select{|img| !File.directory?(img)}.collect do |img| 
-			ImageInfo.new(img_root, img)
-		end
-	end
+class ImageInfo
+  attr_reader :src
+  attr_reader :name
+  def initialize(path, name)
+    @src = File.join(path, name)
+    @name = name
+  end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ Rmms::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  #resources :images do
+  #  put 'upload', :on => :collection
+  #  get 'upload', :on => :collection
+  #end
 
   # Sample resource route with options:
   #   resources :products do
@@ -54,5 +58,5 @@ Rmms::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  match ':controller(/:action(/:id(.:format)))'
 end
