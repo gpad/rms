766c61d679a2bd03beb7845177e7b6602e590241
Gianluca Padovani <gpadovani@gmail.com>
2011-11-22 17:45:42 +0100

    Migliorata visualizzaiozne di un immagine in xml

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -9,11 +9,6 @@ class ImagesController < ApplicationController
   # GET /images/1.xml
   def show
     @image = Image.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @image }
-    end
   end
 
   # GET /images/new
---
+++ app/views/images/show.xml.builder
@@ -0,0 +1,4 @@
+xml.image do |i| 
+  i.id(@image.id)
+  i.name(@image.name)
+end
